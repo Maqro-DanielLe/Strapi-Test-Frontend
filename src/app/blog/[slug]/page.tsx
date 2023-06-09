93ad17f3c34cd7ac7b48305c621c61e6335f3d26
Daniel Le <lehacongduy99@gmail.com>
2023-06-09 14:23:19 +1000

    Added more typing

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,24 +1,45 @@
 import React from 'react'
 import Image from 'next/image'
+import { BlogPost } from '@/data/StrapiType'
 
+type SlugParam = {
+    slug: string,
+}
 
-export default async function SingleBlog({params}:any) {
+type StrapiComponent = {
+    data: {
+        attributes: BlogPost,
+        id: number,
+    }
+}
+
+export default async function SingleBlog({params}: {params:SlugParam}) {
+
     const res = await fetch(`http://127.0.0.1:1337/api/blog-posts/${params.slug}`)
-    const data: any = await res.json()
+    const data: StrapiComponent = await res.json()
 
-    const post: any = data.data.attributes
+    const post: BlogPost = data.data.attributes
 
     return (
-        <div className='text-black mt-32 mb-16 max-w-7xl mx-auto px-24'>
-            <div className='border-2 rounded-lg border-neutral-400 p-10'>
-                <h2 className= 'text-5xl font-extrabold mb-10'>{post.title}</h2>
-                <p className='mb-10'>{post.body}</p>
+        <div className='text-black mt-32 mb-16 max-w-4xl mx-auto px-24'>
+            <div className='border-2 rounded-lg border-neutral-400'>
                 <Image
                     className='w-[100%]' 
                     src={post.imageLink} 
                     alt={post.imageDescription} 
                     width={1000} 
                     height={700}/>
+                <h2 className= 'text-5xl font-extrabold my-10 px-10'>{post.title}</h2>
+                <p className='px-10 mb-10'>{post.body}</p>
+                <div className='flex items-center justify-between mb-2'>
+                    <p className='px-10'>
+                        {new Date(post.date).toDateString()}
+                        {' '}
+                        {new Date(post.date).toLocaleTimeString()}
+                    </p>
+                    <div className='border border-black mr-10 rounded-lg px-3 text-sm'><p>Duration</p></div>
+                </div>
+                
             </div>
             
         </div>
---
+++ src/data/StrapiType.tsx
@@ -0,0 +1,12 @@
+export type BlogPost = {
+    title: string
+    body: string
+    imageLink: string
+    imageDescription: string
+    date: number
+}
+
+export type Response = {
+    data: any,
+    id: number
+}

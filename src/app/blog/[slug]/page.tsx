11297eb859ca63dfc1ba31a89a637f09f1dc85f8
Daniel Le <lehacongduy99@gmail.com>
2023-06-09 13:53:27 +1000

    Added some styling to single blog view

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -9,10 +9,18 @@ export default async function SingleBlog({params}:any) {
     const post: any = data.data.attributes
 
     return (
-        <div className='text-black'>
-            <h2 className=' mt-24 text-5xl font-extrabold mx-auto mb-10'>{post.title}</h2>
-            <p className='mb-10'>{post.body}</p>
-            <Image src={post.imageLink} alt={post.imageDescription} width={1000} height={700}/>
+        <div className='text-black mt-32 mb-16 max-w-7xl mx-auto px-24'>
+            <div className='border-2 rounded-lg border-neutral-400 p-10'>
+                <h2 className= 'text-5xl font-extrabold mb-10'>{post.title}</h2>
+                <p className='mb-10'>{post.body}</p>
+                <Image
+                    className='w-[100%]' 
+                    src={post.imageLink} 
+                    alt={post.imageDescription} 
+                    width={1000} 
+                    height={700}/>
+            </div>
+            
         </div>
     )
 }
